82d79602b6283d6608e9acb067330e254e04700d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-30 19:02:23 +0800

    Add func: Gameboard module and addEventListener public method

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// TODO: 
+// - Create module for: gameBoard
+// - Create factory function for: Player
+// -------------------------------------
+//
+
+const Gameboard = (function(doc){
+  'use strict';
+
+  const addListenerToDivGrid = function(gridCellSelector) {
+    const gridCells = doc.querySelectorAll(gridCellSelector);
+
+    const listenerCallback = function(e) {
+      e.target.classList.toggle("green");
+    }
+
+    gridCells.forEach(gridCell => {
+      gridCell.addEventListener("click", listenerCallback)
+    });
+  } 
+
+  return {
+    addListenerToDivGrid,
+  }
+})(document);
+
+(function(){
+  // NOTE: Main game loop
+  Gameboard.addListenerToDivGrid(".tictacgrid");
+})();
