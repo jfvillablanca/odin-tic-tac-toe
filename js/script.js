150818e100518f52c3f9df127b5b50ebf012d8fa
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-30 19:34:56 +0800

    Apply code formatting

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// TODO: 
+// TODO:
 // - Create module for: gameBoard
 // - Create factory function for: Player
 // -------------------------------------
@@ -9,46 +9,49 @@ const documentMock = (() => ({
   }),
 }))();
 
-const Gameboard = (function(doc){
-  'use strict';
+const Gameboard = (function (doc) {
+  "use strict";
 
-  const addListenerToDivGrid = function(gridCellSelector) {
+  const addListenerToDivGrid = function (gridCellSelector) {
     const gridCells = doc.querySelectorAll(gridCellSelector);
 
-    const listenerCallback = function(e) {
+    const listenerCallback = function (e) {
       e.target.classList.toggle("green");
-    }
+    };
 
-    gridCells.forEach(gridCell => {
-      gridCell.addEventListener("click", listenerCallback)
+    gridCells.forEach((gridCell) => {
+      gridCell.addEventListener("click", listenerCallback);
     });
-  } 
+  };
 
   const createDOMElement = function (asset) {
-    const newDOMElement = doc.createElement(asset["tag"]);    
+    const newDOMElement = doc.createElement(asset["tag"]);
     return {
       newDOMElement,
-    }
-  }
+    };
+  };
 
   return {
     addListenerToDivGrid,
     createDOMElement,
-  }
+  };
 })(document || documentMock);
 
-const assetFactory = function(name, tag, src) {
-  'use strict';
+const assetFactory = function (name, tag, src) {
+  "use strict";
 
   return {
     name,
     tag,
     src,
-
-  }
+  };
 };
 
-(function(){
+(function () {
   // NOTE: Main game loop
+  const cross = assetFactory("cross", "img", "./../images/assets/sword.svg");
+  const circle = assetFactory("cross", "img", "./../images/assets/shield.svg");
+  console.log(Gameboard.createDOMElement(cross));
+  console.log(Gameboard.createDOMElement(circle));
   Gameboard.addListenerToDivGrid(".tictacgrid");
 })();
