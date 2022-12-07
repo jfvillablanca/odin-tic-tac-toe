12f3e171c6c0d9e53dcb1f7db0595efd1ff941c4
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-07 16:41:30 +0800

    Chore: code format

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@
 // - DOM popup winner/draw
 // / Scores
 // - Player choose their game piece
-// - Display whose turn it is
+// / Display whose turn it is (Partial)
 // / Highlight winning cells
 // / Prettify the game board in css
+// NOTE: Check MutationObserver API for the resetgame button problem
 
 const documentMock = (() => ({
   querySelector: (_selector) => ({
@@ -24,7 +25,7 @@ const Gameboard = (function (doc) {
   let _scoreX = 0;
   let _scoreO = 0;
 
-  // NOTE: It takes 5 moves to win the game. 
+  // NOTE: It takes 5 moves to win the game.
   // Example: If player 1 tries to win ASAP and player 2 plays ignorantly,
   // player 1 wins after placing their piece 3 times.
   const MINIMUM_MOVE_COUNT_TO_WIN = 5;
@@ -51,7 +52,7 @@ const Gameboard = (function (doc) {
     _cacheDOM(srcX, srcO);
   };
 
-  const _cacheDOM = function(srcX, srcO) {
+  const _cacheDOM = function (srcX, srcO) {
     _srcAssetX = srcX;
     _srcAssetO = srcO;
     _gamePieceX = doc.createElement("img");
@@ -62,13 +63,14 @@ const Gameboard = (function (doc) {
 
     _gridCells.forEach((_gridCell) => {
       _gridCell.addEventListener("click", _addToGameBoard);
+      _gridCell.classList.add("enabled");
     });
-    
+
     _gameBanner = doc.querySelector(".gamebanner");
     _gameScore = doc.querySelector(".score");
-  }
+  };
 
-  const _render = function() {
+  const _render = function () {
     _gameBanner.textContent = `Your turn: ${_currentGamePiece}`;
     _gameScore.textContent = `X: [${_scoreX}]    O: [${_scoreO}] `;
     //
@@ -76,30 +78,44 @@ const Gameboard = (function (doc) {
       const row = +_gridCell.getAttribute("data-row");
       const column = +_gridCell.getAttribute("data-column");
       const _gamePieceRender = doc.createElement("img");
-      
-      if (_gameBoardArray[row][column] === "X" && _gridCell.firstChild === null) {
+
+      if (
+        _gameBoardArray[row][column] === "X" &&
+        _gridCell.firstChild === null
+      ) {
         _gamePieceRender.setAttribute("src", _gamePieceX.getAttribute("src"));
         _gridCell.appendChild(_gamePieceRender);
-      } else if (_gameBoardArray[row][column] === "O" && _gridCell.firstChild === null) {
+      } else if (
+        _gameBoardArray[row][column] === "O" &&
+        _gridCell.firstChild === null
+      ) {
         _gamePieceRender.setAttribute("src", _gamePieceO.getAttribute("src"));
         _gridCell.appendChild(_gamePieceRender);
-      } 
+      }
     });
-  }
+  };
 
-  const _addToGameBoard = function(_gridCellEvent) {
+  const _addToGameBoard = function (_gridCellEvent) {
     const cellRow = +_gridCellEvent.target.getAttribute("data-row");
     const cellColumn = +_gridCellEvent.target.getAttribute("data-column");
-    
-    // HACK: condition 1 checks if the cell is free, 
+
+    // HACK: condition 1 checks if the cell is free,
     // condition 2 checks if the game has started; see: GameStartReset._startGame
-    if (_gameBoardArray[cellRow][cellColumn] === 0 && _gridCellEvent.target.classList.contains("enabled")) {
+    if (
+      _gameBoardArray[cellRow][cellColumn] === 0 &&
+      _gridCellEvent.target.classList.contains("enabled")
+    ) {
       _gameBoardArray[cellRow].splice(cellColumn, 1, _currentGamePiece);
 
       _render();
 
-      if (_moveCount >= MINIMUM_MOVE_COUNT_TO_WIN && _checkWinCondition(cellRow, cellColumn)) {
-        console.log(`${_currentGamePiece} wins; winning move: [row: ${cellRow}, col: ${cellColumn}]`);
+      if (
+        _moveCount >= MINIMUM_MOVE_COUNT_TO_WIN &&
+        _checkWinCondition(cellRow, cellColumn)
+      ) {
+        console.log(
+          `${_currentGamePiece} wins; winning move: [row: ${cellRow}, col: ${cellColumn}]`
+        );
         // console.log(`movecount: ${_moveCount}`);
         console.table(_gameBoardArray);
         _signalThatMatchFinished(`${_currentGamePiece}`);
@@ -108,11 +124,12 @@ const Gameboard = (function (doc) {
         console.log("Game draw");
         _signalThatMatchFinished("draw");
       }
-    } 
-  }
+      _toggleCurrentGamePiece();
+    }
+  };
 
-  const _signalThatMatchFinished = function(winner) {
-  // const _signalThatMatchFinished = function(winner, _newMatch) {
+  const _signalThatMatchFinished = function (winner) {
+    // const _signalThatMatchFinished = function(winner, _newMatch) {
     if (winner === "X") {
       _scoreX++;
       _gameBanner.textContent = "X wins";
@@ -136,7 +153,7 @@ const Gameboard = (function (doc) {
       }
     });
     Gameboard.init(_srcAssetX, _srcAssetO);
-  }
+  };
 
   const _sleep = function (milliseconds) {
     const date = Date.now();
@@ -146,199 +163,227 @@ const Gameboard = (function (doc) {
     } while (currentDate - date < milliseconds);
   }
 
-  const _toggleCurrentGamePiece = function() {
-    _currentGamePiece === "X" ? _currentGamePiece = "O" : _currentGamePiece = "X";
+  const _toggleCurrentGamePiece = function () {
+    _currentGamePiece === "X"
+      ? (_currentGamePiece = "O")
+      : (_currentGamePiece = "X");
     _moveCount++;
-  }
+  };
 
   const _checkWinCondition = function (i, j) {
     if (i === 1 && j === 1) {
       return _checkIfMiddleIsWinningMove();
-    } else if ((i === 0 && j === 0) ||
-               (i === 0 && j === 2) ||
-               (i === 2 && j === 0) || 
-               (i === 2 && j === 2)) {
+    } else if (
+      (i === 0 && j === 0) ||
+      (i === 0 && j === 2) ||
+      (i === 2 && j === 0) ||
+      (i === 2 && j === 2)
+    ) {
       return _checkIfCornerIsWinningMove(i, j);
     } else {
       return _checkIfEdgeIsWinningMove(i, j);
-    } 
-  }
+    }
+  };
 
-  const _checkIfMiddleIsWinningMove = function() {
+  const _checkIfMiddleIsWinningMove = function () {
     const i = 1;
     const j = 1;
     // to shorten the `if` condition block
     const gBA = _gameBoardArray;
-    
+
     // |   |   |   |
     // |   | * |   |
     // |   |   |   |
-    if (gBA[i][j] === gBA[i-1][j-1] && gBA[i][j] === gBA[i+1][j+1])          {       // \
-      _highlightWinningCells([i,j], [i-1,j-1], [i+1,j+1])
+    if (gBA[i][j] === gBA[i - 1][j - 1] && gBA[i][j] === gBA[i + 1][j + 1]) {
+      // \
+      _highlightWinningCells([i, j], [i - 1, j - 1], [i + 1, j + 1]);
       return true;
-    }
-    else if (gBA[i][j] === gBA[i][j-1]   && gBA[i][j] === gBA[i][j+1])       {       // |
-      _highlightWinningCells([i,j], [i,j-1], [i,j+1])
+    } else if (gBA[i][j] === gBA[i][j - 1] && gBA[i][j] === gBA[i][j + 1]) {
+      // |
+      _highlightWinningCells([i, j], [i, j - 1], [i, j + 1]);
       return true;
-    }
-    else if (gBA[i][j] === gBA[i+1][j-1] && gBA[i][j] === gBA[i-1][j+1])     {       // /
-      _highlightWinningCells([i,j], [i+1,j-1], [i-1,j+1])
+    } else if (
+      gBA[i][j] === gBA[i + 1][j - 1] &&
+      gBA[i][j] === gBA[i - 1][j + 1]
+    ) {
+      // /
+      _highlightWinningCells([i, j], [i + 1, j - 1], [i - 1, j + 1]);
       return true;
-    }
-    else if (gBA[i][j] === gBA[i-1][j]   && gBA[i][j] === gBA[i+1][j])       {       // --     
-      _highlightWinningCells([i,j], [i-1,j], [i+1,j])
+    } else if (gBA[i][j] === gBA[i - 1][j] && gBA[i][j] === gBA[i + 1][j]) {
+      // --
+      _highlightWinningCells([i, j], [i - 1, j], [i + 1, j]);
       return true;
     } else return false;
-  }
+  };
 
-  const _checkIfCornerIsWinningMove = function(i, j) {
+  const _checkIfCornerIsWinningMove = function (i, j) {
     // to shorten the `if` condition block
     const gBA = _gameBoardArray;
-    
+
     // | * |   |   |
     // |   |   |   |
     // |   |   |   |
     if (i === 0 && j === 0) {
-      if (gBA[i][j] === gBA[i][j+1]   && gBA[i][j] === gBA[i][j+2])          {       // --
-        _highlightWinningCells([i,j], [i,j+1], [i,j+2])
+      if (gBA[i][j] === gBA[i][j + 1] && gBA[i][j] === gBA[i][j + 2]) {
+        // --
+        _highlightWinningCells([i, j], [i, j + 1], [i, j + 2]);
         return true;
-      }  
-      else if (gBA[i][j] === gBA[i+1][j+1] && gBA[i][j] === gBA[i+2][j+2])   {       // \
-        _highlightWinningCells([i,j], [i+1,j+1], [i+2,j+2])
+      } else if (
+        gBA[i][j] === gBA[i + 1][j + 1] &&
+        gBA[i][j] === gBA[i + 2][j + 2]
+      ) {
+        // \
+        _highlightWinningCells([i, j], [i + 1, j + 1], [i + 2, j + 2]);
+        return true;
+      } else if (gBA[i][j] === gBA[i + 1][j] && gBA[i][j] === gBA[i + 2][j]) {
+        // |
+        _highlightWinningCells([i, j], [i + 1, j], [i + 2, j]);
         return true;
       }
-      else if (gBA[i][j] === gBA[i+1][j]   && gBA[i][j] === gBA[i+2][j])     {       // |
-        _highlightWinningCells([i,j], [i+1,j], [i+2,j])
-        return true;
-      }
-    } 
-    // |   |   | * |
+    } // |   |   | * |
     // |   |   |   |
     // |   |   |   |
     else if (i === 0 && j === 2) {
-      if (gBA[i][j] === gBA[i][j-1]   && gBA[i][j] === gBA[i][j-2])          {       // --
-        _highlightWinningCells([i,j], [i,j-1], [i,j-2])
+      if (gBA[i][j] === gBA[i][j - 1] && gBA[i][j] === gBA[i][j - 2]) {
+        // --
+        _highlightWinningCells([i, j], [i, j - 1], [i, j - 2]);
         return true;
-      }  
-      else if (gBA[i][j] === gBA[i+1][j-1] && gBA[i][j] === gBA[i+2][j-2])   {       // /
-        _highlightWinningCells([i,j], [i+1,j-1], [i+2,j-2])
+      } else if (
+        gBA[i][j] === gBA[i + 1][j - 1] &&
+        gBA[i][j] === gBA[i + 2][j - 2]
+      ) {
+        // /
+        _highlightWinningCells([i, j], [i + 1, j - 1], [i + 2, j - 2]);
+        return true;
+      } else if (gBA[i][j] === gBA[i + 1][j] && gBA[i][j] === gBA[i + 2][j]) {
+        // |
+        _highlightWinningCells([i, j], [i + 1, j], [i + 2, j]);
         return true;
       }
-      else if (gBA[i][j] === gBA[i+1][j]   && gBA[i][j] === gBA[i+2][j])     {       // |
-        _highlightWinningCells([i,j], [i+1,j], [i+2,j])
-        return true;
-      }
-    } 
-    // |   |   |   |
+    } // |   |   |   |
     // |   |   |   |
     // | * |   |   |
     else if (i === 2 && j === 0) {
-      if (gBA[i][j] === gBA[i][j+1]   && gBA[i][j] === gBA[i][j+2])          {       // --
-        _highlightWinningCells([i,j], [i,j+1], [i,j+2])
+      if (gBA[i][j] === gBA[i][j + 1] && gBA[i][j] === gBA[i][j + 2]) {
+        // --
+        _highlightWinningCells([i, j], [i, j + 1], [i, j + 2]);
         return true;
-      }  
-      else if (gBA[i][j] === gBA[i-1][j+1] && gBA[i][j] === gBA[i-2][j+2])   {       // /
-        _highlightWinningCells([i,j], [i-1,j+1], [i-2,j+2])
+      } else if (
+        gBA[i][j] === gBA[i - 1][j + 1] &&
+        gBA[i][j] === gBA[i - 2][j + 2]
+      ) {
+        // /
+        _highlightWinningCells([i, j], [i - 1, j + 1], [i - 2, j + 2]);
+        return true;
+      } else if (gBA[i][j] === gBA[i - 1][j] && gBA[i][j] === gBA[i - 2][j]) {
+        // |
+        _highlightWinningCells([i, j], [i - 1, j], [i - 2, j]);
         return true;
       }
-      else if (gBA[i][j] === gBA[i-1][j]   && gBA[i][j] === gBA[i-2][j])     {       // |
-        _highlightWinningCells([i,j], [i-1,j], [i-2,j])
-        return true;
-      }
-    } 
-    // |   |   |   |
+    } // |   |   |   |
     // |   |   |   |
     // |   |   | * |
     else {
-      if (gBA[i][j] === gBA[i][j-1]   && gBA[i][j] === gBA[i][j-2])          {       // --
-        _highlightWinningCells([i,j], [i,j-1], [i,j-2])
+      if (gBA[i][j] === gBA[i][j - 1] && gBA[i][j] === gBA[i][j - 2]) {
+        // --
+        _highlightWinningCells([i, j], [i, j - 1], [i, j - 2]);
         return true;
-      }  
-      else if (gBA[i][j] === gBA[i-1][j-1] && gBA[i][j] === gBA[i-2][j-2])   {       // \
-        _highlightWinningCells([i,j], [i-1,j-1], [i-2,j-2])
+      } else if (
+        gBA[i][j] === gBA[i - 1][j - 1] &&
+        gBA[i][j] === gBA[i - 2][j - 2]
+      ) {
+        // \
+        _highlightWinningCells([i, j], [i - 1, j - 1], [i - 2, j - 2]);
         return true;
-      }
-      else if (gBA[i][j] === gBA[i-1][j]   && gBA[i][j] === gBA[i-2][j])     {       // |
-        _highlightWinningCells([i,j], [i-1,j], [i-2,j])
+      } else if (gBA[i][j] === gBA[i - 1][j] && gBA[i][j] === gBA[i - 2][j]) {
+        // |
+        _highlightWinningCells([i, j], [i - 1, j], [i - 2, j]);
         return true;
-      } else return false;  
+      } else return false;
     }
-  }
+  };
 
-  const _checkIfEdgeIsWinningMove = function(i, j) {
+  const _checkIfEdgeIsWinningMove = function (i, j) {
     // to shorten the `if` condition block
     const gBA = _gameBoardArray;
-    
+
     // |   |   |   |
     // | * |   |   |
     // |   |   |   |
     if (i === 1 && j === 0) {
-      if (gBA[i][j] === gBA[i][j+1]   && gBA[i][j] === gBA[i][j+2])         {        // --
-        _highlightWinningCells([i,j], [i,j+1], [i,j+2])
+      if (gBA[i][j] === gBA[i][j + 1] && gBA[i][j] === gBA[i][j + 2]) {
+        // --
+        _highlightWinningCells([i, j], [i, j + 1], [i, j + 2]);
         return true;
-      }  
-      else if (gBA[i][j] === gBA[i-1][j]   && gBA[i][j] === gBA[i+1][j])    {        // |
-        _highlightWinningCells([i,j], [i-1,j], [i+1,j])
+      } else if (gBA[i][j] === gBA[i - 1][j] && gBA[i][j] === gBA[i + 1][j]) {
+        // |
+        _highlightWinningCells([i, j], [i - 1, j], [i + 1, j]);
         return true;
       }
-    } 
-    // |   | * |   |
+    } // |   | * |   |
     // |   |   |   |
     // |   |   |   |
     else if (i === 0 && j === 1) {
-      if (gBA[i][j] === gBA[i][j-1]   && gBA[i][j] === gBA[i][j+1])         {        // --
-        _highlightWinningCells([i,j], [i,j-1], [i,j+1])
+      if (gBA[i][j] === gBA[i][j - 1] && gBA[i][j] === gBA[i][j + 1]) {
+        // --
+        _highlightWinningCells([i, j], [i, j - 1], [i, j + 1]);
         return true;
-      }  
-      else if (gBA[i][j] === gBA[i+1][j]   && gBA[i][j] === gBA[i+2][j])    {        // |
-        _highlightWinningCells([i,j], [i+1,j], [i+2,j])
+      } else if (gBA[i][j] === gBA[i + 1][j] && gBA[i][j] === gBA[i + 2][j]) {
+        // |
+        _highlightWinningCells([i, j], [i + 1, j], [i + 2, j]);
         return true;
       }
-    } 
-    // |   |   |   |
+    } // |   |   |   |
     // |   |   | * |
     // |   |   |   |
     else if (i === 1 && j === 2) {
-      if (gBA[i][j] === gBA[i][j-1]   && gBA[i][j] === gBA[i][j-2])         {        // --
-        _highlightWinningCells([i,j], [i,j-1], [i,j-2])
+      if (gBA[i][j] === gBA[i][j - 1] && gBA[i][j] === gBA[i][j - 2]) {
+        // --
+        _highlightWinningCells([i, j], [i, j - 1], [i, j - 2]);
         return true;
-      }  
-      else if (gBA[i][j] === gBA[i-1][j]   && gBA[i][j] === gBA[i+1][j])    {        // |
-        _highlightWinningCells([i,j], [i-1,j], [i+1,j])
+      } else if (gBA[i][j] === gBA[i - 1][j] && gBA[i][j] === gBA[i + 1][j]) {
+        // |
+        _highlightWinningCells([i, j], [i - 1, j], [i + 1, j]);
         return true;
       }
-    } 
-    // |   |   |   |
+    } // |   |   |   |
     // |   |   |   |
     // |   | * |   |
     else {
-      if (gBA[i][j] === gBA[i][j-1]   && gBA[i][j] === gBA[i][j+1])         {        // --
-        _highlightWinningCells([i,j], [i,j-1], [i,j+1])
+      if (gBA[i][j] === gBA[i][j - 1] && gBA[i][j] === gBA[i][j + 1]) {
+        // --
+        _highlightWinningCells([i, j], [i, j - 1], [i, j + 1]);
         return true;
-      }  
-      else if (gBA[i][j] === gBA[i-1][j]   && gBA[i][j] === gBA[i-2][j])    {        // |
-        _highlightWinningCells([i,j], [i-1,j], [i-2,j])
+      } else if (gBA[i][j] === gBA[i - 1][j] && gBA[i][j] === gBA[i - 2][j]) {
+        // |
+        _highlightWinningCells([i, j], [i - 1, j], [i - 2, j]);
         return true;
-      } else return false;  
-    } 
-  }
+      } else return false;
+    }
+  };
 
-  const _highlightWinningCells = function(cell1, cell2, cell3) {
-    const gridCell1 = doc.querySelector(`.tictacgrid[data-row="${cell1[0]}"][data-column="${cell1[1]}"]`);
-    const gridCell2 = doc.querySelector(`.tictacgrid[data-row="${cell2[0]}"][data-column="${cell2[1]}"]`);
-    const gridCell3 = doc.querySelector(`.tictacgrid[data-row="${cell3[0]}"][data-column="${cell3[1]}"]`);
+  const _highlightWinningCells = function (cell1, cell2, cell3) {
+    const gridCell1 = doc.querySelector(
+      `.tictacgrid[data-row="${cell1[0]}"][data-column="${cell1[1]}"]`
+    );
+    const gridCell2 = doc.querySelector(
+      `.tictacgrid[data-row="${cell2[0]}"][data-column="${cell2[1]}"]`
+    );
+    const gridCell3 = doc.querySelector(
+      `.tictacgrid[data-row="${cell3[0]}"][data-column="${cell3[1]}"]`
+    );
     gridCell1.classList.add("win");
     gridCell2.classList.add("win");
     gridCell3.classList.add("win");
     return;
-  }
+  };
 
   return {
     init,
-  }
+  };
 })(document || documentMock);
 
-const GameStartReset = (function(doc) {
+const GameStartReset = (function (doc) {
   "use strict";
 
   let _popupWindow;
@@ -347,26 +392,26 @@ const GameStartReset = (function(doc) {
   let _gridCells;
   let _gameBanner;
 
-  const init = function(srcX, srcO) {
+  const init = function (srcX, srcO) {
     _cacheDOM();
     Gameboard.init(srcX, srcO);
-  }
+  };
 
-  const _cacheDOM = function() {
+  const _cacheDOM = function () {
     _popupWindow = doc.querySelector(".popup");
-    _startGameButton = doc.querySelector(".popup > button"); 
+    _startGameButton = doc.querySelector(".popup > button");
     _resetGameButton = doc.querySelector(".container > :last-child");
     _gameBanner = doc.querySelector(".gamebanner");
     _gridCells = doc.querySelectorAll(".tictacgrid");
     _resetGameButton.classList.add("hidden");
     _initializeGame();
-  } 
+  };
 
-  const _initializeGame = function() {
+  const _initializeGame = function () {
     _gameBanner.textContent = "Hello player";
 
     _startGameButton.addEventListener("click", () => {
-      _popupWindow.classList.add("hidden");  
+      _popupWindow.classList.add("hidden");
       _resetGameButton.classList.remove("hidden");
 
       // HACK: the .enabled class is a toggle to see
@@ -374,15 +419,15 @@ const GameStartReset = (function(doc) {
       _gridCells.forEach((gridCell) => {
         gridCell.classList.add("enabled");
       });
-    })
+    });
 
     _resetGameButton.addEventListener("click", () => {
       _resetGame();
-    })
-  }
+    });
+  };
 
-  const _resetGame = function() {
-    _popupWindow.classList.remove("hidden");  
+  const _resetGame = function () {
+    _popupWindow.classList.remove("hidden");
     _resetGameButton.classList.add("hidden");
 
     _gridCells.forEach((gridCell) => {
@@ -395,11 +440,11 @@ const GameStartReset = (function(doc) {
       }
     });
     gameLoop();
-  }
+  };
 
   return {
     init,
-  }
+  };
 })(document || documentMock);
 
 // NOTE: Main game loop
@@ -409,5 +454,5 @@ const gameLoop = function () {
 
   GameStartReset.init(srcCross, srcCircle);
   // Gameboard.init(srcCross, srcCircle);
-}
+};
 gameLoop();
