63c648b8a37e7a8ca0cfbc207fafcd2d5c91d223
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-08 12:56:46 +0800

    Move image assets

--- js/script.js
+++ js/script.js
@@ -438,8 +438,8 @@ const Gameboard = (function (doc) {
 
 // NOTE: Main game loop
 const gameLoop = function () {
-  const srcCross = "./../images/assets/sword.SVG";
-  const srcCircle = "./../images/assets/shield.SVG";
+  const srcCross = "./../images/sword.svg";
+  const srcCircle = "./../images/shield.svg";
 
   Gameboard.init(srcCross, srcCircle);
 };
